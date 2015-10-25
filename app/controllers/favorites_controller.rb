8a78689f19359922dd42f84d3def26a83d874c29
matsukenta <takeshi.matsuura@axas-japan.co.jp>
2015-10-25 14:01:31 +0000

    kadai kaminari favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,14 +3,14 @@ class FavoritesController < ApplicationController
     
     #お気に入り登録
     def create
-        @micropost = Micropost.find(params[:favmicropost_id])
-        current_user.favorite(@micropost)
+        @user = User.find(params[:favmicropost_id])
+        current_user.favorite(@user)
     end
     
     #お気に入り削除
     def destroy
-        @micropost = current_user.favoriting_favorites.find(params[:id]).favmicropost
-        current_user.unfavorite(@micropost)
+        @user = current_user.favoriting_favorites.find(params[:id]).favmicropost
+        current_user.unfavorite(@user)
     end
     
 end
--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,7 +7,7 @@ class MicropostsController < ApplicationController
             flash[:success] = "Micropost created!"
             redirect_to root_url
         else
-            @feed_items = current_user.feed_items.in(:user).order(created_at: :desc)
+            @feed_items = current_user.feed_items.in(:user).order(created_at: :desc).page(params[:page]).per(5)
             render 'static_pages/home'
         end
     end
